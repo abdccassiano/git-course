6d7a7c71d08b7d81ee84d6930cdc4b5aa4d8fb9c
Abi Cass <abdccassiano@hotmail.com>
2021-12-13 14:06:09 -0600

    Update app.py

--- src/app.py
+++ src/app.py
@@ -14,3 +14,10 @@ def greating():
 def sum(a: int, b: int):
     nums_sum = a + b
     return f"La suma es: {str(nums_sum)}";
+
+@@app.route('/multiply/<int:a></int:b>')
+def multiply(a: int, b: int):
+    result = float(a * b)
+    return f"La multiplicaicón es: {str(result)}";
+
+
